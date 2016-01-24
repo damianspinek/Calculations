949475ee93fbcd36adeb5b6865f71773a0d5c9d9
Damian Spinek <damian00737@gmail.com>
2016-01-24 14:22:10 +0100

    Add tests

---
+++ src/main/java/Calculations/Calculations/AppTest.java
@@ -0,0 +1,13 @@
+package Calculations.Calculations;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void test(){
+		assertEquals(true, true);
+	}
+}
