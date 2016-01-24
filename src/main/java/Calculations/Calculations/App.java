1d3d415f9550465ae5084d95b36f37025bc116e4
Damian Spinek <damian00737@gmail.com>
2016-01-24 14:16:24 +0100

    project config

---
+++ src/main/java/Calculations/Calculations/App.java
@@ -0,0 +1,13 @@
+package Calculations.Calculations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
