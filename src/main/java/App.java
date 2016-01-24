b894ad29a1afd675bfb934d5faad66eac17065db
Damian Spinek <damian00737@gmail.com>
2016-01-24 16:00:01 +0100

    Exam result

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,2 @@
+package Calculations.Calculations;
 public class App{     public static int plus(int a, int b){return a+b;}     public static int minus(int a, int b){return a-b;}     public static int divide(int a, int b){return a/b;}     public static int multiple(int a, int b){return a*b;} }
