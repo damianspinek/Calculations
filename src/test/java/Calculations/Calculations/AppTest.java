8717320e66b76745e38d35f0b4ebbf06e2666709
Damian Spinek <damian00737@gmail.com>
2016-01-24 15:28:41 +0100

    move tests

---
+++ src/test/java/Calculations/Calculations/AppTest.java
@@ -0,0 +1,18 @@
+package Calculations.Calculations;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+	
+	@Test
+	public void test(){
+		assertEquals(true, true);
+	}
+	
+	@Test
+	public void test_wrong(){
+		assertEquals(true, false);
+	}
+}
