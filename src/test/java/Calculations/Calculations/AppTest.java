9701c31743718b429ff912b786419326948b814d
Damian Spinek <damian00737@gmail.com>
2016-01-24 15:45:11 +0100

    Add good tests

--- src/test/java/Calculations/Calculations/AppTest.java
+++ src/test/java/Calculations/Calculations/AppTest.java
@@ -7,12 +7,22 @@ import org.junit.Test;
 public class AppTest {
 	
 	@Test
-	public void test(){
-		assertEquals(true, true);
+	public void plus_test(){
+		assertEquals(4, App.plus(2,2));
 	}
 	
 	@Test
-	public void test_wrong(){
-		assertEquals(true, true);
+	public void minus_test(){
+		assertEquals(0, App.minus(2,2));
+	}
+	
+	@Test
+	public void divide_test(){
+		assertEquals(1, App.divide(2,2));
+	}
+	
+	@Test
+	public void multiple_test(){
+		assertEquals(4, App.multiple(2,2));
 	}
 }
